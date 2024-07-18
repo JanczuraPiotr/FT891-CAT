a85b602b8e0d1947e6f593e20cfc0dc101fca570
Piotr Janczura <piotr@janczura.pl>
2024-07-18 21:21:55 +0200

    Struktura dla testów.

---
+++ cat-server/tests/BaseTest.cpp
@@ -0,0 +1,11 @@
+//
+// Created by piotr@janczura.pl on 2024.07.18
+//
+
+#include "BaseTest.h"
+
+
+TEST_F(BaseTest, testOk)
+{
+
+}
---
+++ cat-server/tests/BaseTest.h
@@ -0,0 +1,15 @@
+//
+// Created by piotr@janczura.pl on 2024.07.18
+//
+
+#ifndef CAT_SERVER_BASETEST_H
+#define CAT_SERVER_BASETEST_H
+
+#include <gtest/gtest.h>
+
+class BaseTest: public ::testing::Test {
+
+};
+
+
+#endif //CAT_SERVER_BASETEST_H
